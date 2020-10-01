e3df6d0ed6b735a420252289d1ac01bbdeaa5ad3
MWLeonel <wmleonel6@gmail.com>
2020-10-01 12:38:13 -0300

    Mostrar numeros negativos

---
+++ Negativo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Negativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor;
+
+            Console.Write("Digite um número: ");
+            valor = int.Parse(Console.ReadLine());
+
+            if (valor <= 0)
+            {
+                Console.WriteLine("Você digitou um número negativo!");
+            }
+
+            Console.Write("\n");
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+        }
+    }
+}
